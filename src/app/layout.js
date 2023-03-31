4397cb3bf1cc5ffda2bd5845402840e934baa45e
Tobias <juantobiasdl7@gmail.com>
2023-03-30 21:17:30 -0600

    I added signup, signin, signout functions

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,9 +5,33 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
 
+  const signUp = () => {
+    supabase.auth.signUp({
+      email: '',
+      password: '',
+    });
+  };
+
+  const signIn = () => {
+    supabase.auth.signInWithPassword({
+      email: '',
+      password: '',
+    });
+  };
+
+  const signOut = () => {
+    supabase.auth.signOut();
+  };
+
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <button onClick={signUp}>Sign Up</button>
+        <button onClick={signIn}>Sign In</button>
+        <button onClick={signOut}>Sign Out</button>
+        {children}
+      </body>
     </html>
   )
 }
